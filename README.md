1217cd87a4efd21e48e065659611fcc0f394a836
David McElhill <david@white.space>
2025-03-07 13:11:30 +0000

    Added argparser

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-## Hosted LLM 
+## Hosted LLM
+
+Functions for downloading models from HuggingFace and deploying to AWS Sagemaker.
+
+
+### Setup
+1. create a .env file at the project root and populate with Sagemaker config.
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import tempfile
 
@@ -8,6 +9,9 @@ from huggingface import HuggingFace
 from s3 import S3
 from sage_maker import SageMaker
 from utils import get_model_dir_path
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+MODELS_DIR = os.path.join(PROJECT_ROOT, "models")
 
 
 def save_and_upload_model_via_tempdir(model_name: str):
@@ -26,24 +30,27 @@ def save_and_upload_model_via_tempdir(model_name: str):
     logger.info(f"Save and upload model '{model_name}' via tempdir - done")
 
 
-def save_model_to_disk(model_name: str, dir_path: str):
-    logger.info(f"Save model '{model_name}' to '{dir_path}' - start")
-    HuggingFace.save_model(model_name, dir_path)
-    logger.debug(f"Weightings for model '{model_name}' saved to '{dir_path}' - start")
-    HuggingFace.save_tokenizer(model_name, dir_path)
-    logger.debug(f"Tokenizer for model '{model_name}' saved to '{dir_path}' - start")
-    logger.info(f"Save model '{model_name}' to '{dir_path}' - done")
+def save_model_to_disk(model_name: str, models_dir: str = MODELS_DIR):
+    """Save a model files into a model-named sub-directory within the models directory"""
+    model_dir = os.path.join(models_dir, model_name)
+    os.makedirs(model_dir, exist_ok=True)
+    logger.info(f"Save model '{model_name}' to '{model_dir}' - start")
+    HuggingFace.save_model(model_name, model_dir)
+    logger.debug(f"Weightings for model '{model_name}' saved to '{model_dir}' - start")
+    HuggingFace.save_tokenizer(model_name, model_dir)
+    logger.debug(f"Tokenizer for model '{model_name}' saved to '{model_dir}' - start")
+    logger.info(f"Save model '{model_name}' to '{model_dir}' - done")
 
 
-def upload_model_from_disk(model_name: str, dir_path: str):
-    logger.info(f"Upload model '{model_name}' from '{dir_path}' - start")
+def upload_model_from_disk(model_name: str, model_dir: str):
+    logger.info(f"Upload model '{model_name}' from '{model_dir}' - start")
     s3 = S3()
-    for root, dirs, files in os.walk(dir_path):
+    for root, dirs, files in os.walk(model_dir):
         for file in files:
             file_path = os.path.join(root, file)
             s3_key = f"{get_model_dir_path(model_name)}/{file}"
             s3.upload_file(file_path, Config.SM_BUCKET, s3_key)
-    logger.info(f"Upload model '{model_name}' from '{dir_path}' - done")
+    logger.info(f"Upload model '{model_name}' from '{model_dir}' - done")
 
 
 def sagemaker_create_model(model_name: str):
@@ -51,18 +58,37 @@ def sagemaker_create_model(model_name: str):
     print(response)
 
 
-def main(func_name, model_name: str, *args, **kwargs):
+def main(func_name, model_name: str, models_dir: str = None, model_dir: str = None):
     func_map = {
         "save_and_upload_model_via_tempdir": save_and_upload_model_via_tempdir,
         "save_model_to_disk": save_model_to_disk,
         "upload_model_from_disk": upload_model_from_disk,
         "sagemaker_create_model": sagemaker_create_model,
     }
-    return func_map[func_name](model_name, *args, **kwargs)
+
+    if func_name not in func_map:
+        raise ValueError(f"function name '{func_name}' is not recognised")
+
+    func_map[func_name](model_name, models_dir=models_dir, model_dir=model_dir)
 
 
 if __name__ == "__main__":
-    FUNC = "save_model_to_disk"
-    MODEL_NAME = "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B"
-    kwargs = {"dir_path": "/Users/davidmcelhill/Documents/models"}
-    main(FUNC, MODEL_NAME, **kwargs)
+    # python main.py save_model_to_disk deepseek-ai/DeepSeek-R1-Distill-Qwen-14B
+    parser = argparse.ArgumentParser(description="CLI for invoking main with a function name and optional arguments.")
+
+    parser.add_argument("func_name", type=str, help="Name of the function to execute")
+    parser.add_argument(
+        "model_name",
+        type=str,
+        help="Full name of the model on HuggingFace, e.g. deepseek-ai/DeepSeek-R1-Distill-Qwen-14B"
+    )
+    parser.add_argument("--models_dir", type=str, help="Optional 'models' directory path where all models are stored")
+    parser.add_argument(
+        "--model_dir",
+        type=str,
+        help="Optional 'model' directory path where the files can be found for a specific model"
+    )
+
+    args = parser.parse_args()
+
+    main(args.func_name, args.model_name, args.models_dir, args.model_dir)
